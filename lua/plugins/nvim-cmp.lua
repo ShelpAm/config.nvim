dcb48bb497a2bbfcd74e7d21940f5b4805c1bfec
small_sheep_ <1178550325@qq.com>
2023-02-22 00:19:41 +0800

    chore: update

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -5,9 +5,10 @@ return {
     event = { "BufReadPre", "BufNewFile" },
     dependencies = {
       'neovim/nvim-lspconfig',
+      'onsails/lspkind.nvim',
       'hrsh7th/cmp-buffer',
       'hrsh7th/cmp-cmdline',
-      'petertriho/cmp-git',
+      'davidsierradz/cmp-conventionalcommits',
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-path',
       'hrsh7th/cmp-vsnip',
@@ -17,10 +18,10 @@ return {
       -- Mappings.
       -- See `:help vim.diagnostic.*` for documentation on any of the below functions
       local opts = { noremap = true, silent = true }
-      vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+      vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
       vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
       vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-      --vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts) -- TODO conflicts with keymaps.lua
+      --vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, opts) -- TODO conflicts with keymaps.lua
 
       -- Use an on_attach function to only map the following keys
       -- after the language server attaches to the current buffer
@@ -31,18 +32,19 @@ return {
         -- Mappings.
         -- See `:help vim.lsp.*` for documentation on any of the below functions
         local bufopts = { noremap = true, silent = true, buffer = bufnr }
+        vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
         vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
         vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
         vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
         vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-        vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-        vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-        vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-        vim.keymap.set('n', '<space>wl', function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, bufopts)
-        vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-        vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-        vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
         vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+        vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
+        --vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+        --vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workleader_folder, bufopts)
+        --vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workleader_folder, bufopts)
+        --vim.keymap.set('n', '<leader>wl', function() print(vim.inspect(vim.lsp.buf.list_workleader_folders())) end,
+        --bufopts)
+        --vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, bufopts)
       end
 
       local lsp_servers = require('config.lsp')
@@ -78,8 +80,57 @@ return {
         },
       }
 
+      local lspkind = require('lspkind')
+      lspkind.init({
+        -- defines how annotations are shown
+        -- default: symbol
+        -- options: 'text', 'text_symbol', 'symbol_text', 'symbol'
+        mode = 'symbol_text',
+        -- default symbol map
+        -- can be either 'default' (requires nerd-fonts font) or
+        -- 'codicons' for codicon preset (requires vscode-codicons font)
+        -- default: 'default'
+        preset = 'codicons',
+        -- override preset symbols
+        -- default: {}
+        symbol_map = {
+          Text = "",
+          Method = "",
+          Function = "",
+          Constructor = "",
+          Field = "",
+          Variable = "",
+          Class = "ﴯ",
+          Interface = "",
+          Module = "",
+          Property = "ﰠ",
+          Unit = "",
+          Value = "",
+          Enum = "",
+          Keyword = "",
+          Snippet = "",
+          Color = "",
+          File = "",
+          Reference = "",
+          Folder = "",
+          EnumMember = "",
+          Constant = "",
+          Struct = "",
+          Event = "",
+          Operator = "",
+          TypeParameter = ""
+        },
+        menu = {
+          buffer = "[Buffer]",
+          nvim_lsp = "[LSP]",
+          luasnip = "[LuaSnip]",
+          nvim_lua = "[Lua]",
+          latex_symbols = "[Latex]",
+        },
+      })
+
       local cmp = require('cmp')
-      cmp.setup {
+      cmp.setup({
         snippet = {
           -- REQUIRED - you must specify a snippet engine
           expand = function(args)
@@ -90,32 +141,33 @@ return {
           -- completion = cmp.config.window.bordered(),
           -- documentation = cmp.config.window.bordered(),
         },
-        mapping = cmp.mapping.preset.insert(
-          {
-            ['<C-b>'] = cmp.mapping.scroll_docs( -4),
-            ['<C-f>'] = cmp.mapping.scroll_docs(4),
-            --TODO
-            ['<S-Tab>'] = cmp.mapping.select_prev_item(),
-            ['<tab>'] = cmp.mapping.select_next_item(),
-            ['<C-space>'] = cmp.mapping.complete(),
-            ['<C-e>'] = cmp.mapping.abort(),
-            -- Accept currently selected item. Set `select` to `false` to only confirm
-            -- explicitly selected items.
-            ['<CR>'] = cmp.mapping.confirm({ select = true }),
-          }
-        ),
+        mapping = cmp.mapping.preset.insert({
+          ['<C-b>'] = cmp.mapping.scroll_docs( -4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+          ['<tab>'] = cmp.mapping.select_next_item(),
+          ['<C-space>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.abort(),
+          -- Accept currently selected item. Set `select` to `false` to only confirm
+          -- explicitly selected items.
+          ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        }),
         sources = cmp.config.sources({
           { name = 'nvim_lsp' },
           { name = 'vsnip' }, -- For vsnip users.
         }, {
           { name = 'buffer' },
-        })
-      }
+          { name = 'path' },
+        }),
+        formatting = {
+          format = lspkind.cmp_format(),
+        },
+      })
 
       -- Set configuration for specific filetype.
       cmp.setup.filetype('gitcommit', {
         sources = cmp.config.sources({
-          { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+          { name = 'conventionalcommits' },
         }, {
           { name = 'buffer' },
         })
